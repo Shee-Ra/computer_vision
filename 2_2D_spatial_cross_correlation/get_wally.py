a272180b67fa89dcbbe2152d1970e7ee4ff508fd
Dr Mac <shee.ra.codes@gmail.com>
2020-10-10 10:48:36 +1100

    updated 2D xc files

--- 2_2D_spatial_cross_correlation/get_wally.py
+++ 2_2D_spatial_cross_correlation/get_wally.py
@@ -1,15 +1,17 @@
 import numpy as np
 from pyprojroot import here
 import time
-import os
 import logging
 from PIL import Image
 from matplotlib import image, pyplot
+import os
+from itertools import permutations
+
 
 # ------------------------
 # sort out logging
 # ------------------------
-logging.basicConfig(level=logging.DEBUG,     # set level
+logging.basicConfig(level=logging.INFO,     # set level
                     format='%(levelname)s '  # logging level
                     'on line %(lineno)d: '   # line ID
                     '%(message)s '           # message
@@ -41,6 +43,7 @@ def CalcNormedCrossCorr2D(pat, temp):
         log.debug(f'temp now has dimensions: {temp.shape}')
     else:
         pass
+
     # calulate normalised cross correlation
     norm=np.sqrt(((pat**2).sum()) * ((temp**2).sum()))
     log.debug(f'temp and pat dimensions: {temp.shape, pat.shape}')
@@ -59,7 +62,12 @@ def CheckImage(np_array):
     is used for manual checks throughout the code'''
     to_plot=Image.fromarray(np_array) # convert numpy array to image
     pyplot.imshow(to_plot) 
-    pyplot.show()   
+    pyplot.show() 
+
+def ConvertRGBAtoGreyscale(im_array):
+    '''converts an RGBA array into greyscale array'''
+    gs_array=np.dot(im_array[...,:3],[0.299, 0.587, 0.114])
+    return(gs_array)  
 
 def GetLocation(small_image='2_2D_spatial_cross_correlation/data/wallypuzzle_rocket_man.png',
                 big_image='2_2D_spatial_cross_correlation/data/wallypuzzle.png'):  
@@ -94,6 +102,7 @@ def GetLocation(small_image='2_2D_spatial_cross_correlation/data/wallypuzzle_roc
     indices_of_selected_pixel_in_template = np.where(np.all(template_array == selected_pixel, axis=2))
 
     # Now for all (x,y's) in the above, get 2D arrays (rows,cols) in the maze and compare with the strip solid_row 
+    ccmax=0
     for i in range(indices_of_selected_pixel_in_template[0].shape[0]):
         r=indices_of_selected_pixel_in_template[0][i] # row in maze same colour as selected_pixel
         c=indices_of_selected_pixel_in_template[1][i] # columns in maze same colour as selected_pixel
@@ -118,12 +127,26 @@ def GetLocation(small_image='2_2D_spatial_cross_correlation/data/wallypuzzle_roc
                 r_top_maze=r_bottom_maze+pattern_array.shape[0]
                 c_left_maze=c-(count_of_transparent_pixels_to_left+selected_column)
                 c_right_maze=c_left_maze+pattern_array.shape[1]
+                
                 # check image
-                log.info(f'Rocketman found at lag {(r_top_maze,c_left_maze)}.')
-                # CheckImage(template_array[r_bottom_maze:r_top_maze,c_left_maze:c_right_maze,:])
+                search_area = [i for i in permutations([-2,-1,0,1,-2],2)]
+                for indices in search_area:
+                    gs_temp=ConvertRGBAtoGreyscale(template_array[r_bottom_maze+indices[0]:r_top_maze+indices[0],c_left_maze+indices[1]:c_right_maze+indices[1],:])
+                    gs_pat=ConvertRGBAtoGreyscale(pattern_array)
+                    # CheckImage(gs_temp)
+                    # CheckImage(gs_pat)
+                    cc=CalcNormedCrossCorr2D(gs_temp,gs_pat)
+                    if ccmax<cc:
+                        ccmax=cc
+                        coords=(r_bottom_maze+indices[0],r_top_maze+indices[0],c_left_maze+indices[1],c_right_maze+indices[1])
+                log.info(f'Rocketman found at lag {(coords[1],coords[2])}.\n bottom row: {coords[0]}, top row: {coords[1]}, col left: {coords[2]}, col right: {coords[3]}')
+                #CheckImage(template_array[r_bottom_maze:r_top_maze,c_left_maze:c_right_maze,:])
+                return((coords[1],coords[3]))
 
 start=time.time()
 GetLocation(small_image='2_2D_spatial_cross_correlation/data/wallypuzzle_rocket_man.png',
                 big_image='2_2D_spatial_cross_correlation/data/wallypuzzle.png')
 end=time.time()
 log.info(f'Time taken: {end-start}s')
+
+# bottom row:528, top row: 651, col left:981, col right: 1092 
